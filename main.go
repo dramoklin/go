4153f63760383fe7ab2a273a0b1691b0805a5c7f
Nikita <nikita_chernay@ukr.net>
2022-06-06 10:25:01 +0300

    main

--- main.go
+++ main.go
@@ -1,69 +1,15 @@
 package main
 
-import (
-	"fmt"
-)
-
-var radius float32
-
-var x int
-
-var y int
-
-var l int
+import "fmt"
 
 func main() {
-	// var p *int
-	// x := 10
-	// fmt.Println(x)
+	arrayRevers := [3]int{1, 2, 5}
 
-	// increment(p)
+	for i := len(arrayRevers)/3 - 1; i >= 0; i-- {
+		opp := len(arrayRevers) - 1 - i
+		arrayRevers[i], arrayRevers[opp] = arrayRevers[opp], arrayRevers[i]
+	}
 
-	// fmt.Println(p)
-
-	// calculatecirculeArea(2)
-
-	// printcirculeArea(2)
-
-	calculateSquareArea(5, 6)
-
-}
-
-// func increment(p int) {
-// 	p += 1
-// 	return
-
-// }
-
-// func calculatecirculeArea(radius int) (float32, error) {
-// 	if radius <= 0 {
-// 		return float32(0), errors.New("Радиус должен быть отрицательным")
-// 	}
-// 	return float32(radius) * float32(radius) * math.Pi, nil
-
-// }
-
-// func printcirculeArea(radius int) {
-
-// 	fmt.Printf("Радиус круга: %d см. \n", radius)
-// 	fmt.Println("Формула для расчета площади круга: S = π × r2")
-
-// 	circuleArea, err := calculatecirculeArea(radius)
-// 	if err != nil {
-// 		fmt.Println(err.Error())
-// 		return
-
-// 	}
-
-// 	fmt.Printf("Площадь круга: %f32 см. кв. \n", circuleArea)
-
-// }
-
-func calculateSquareArea(x int, y int) {
-
-	var l = x * y
-
-	fmt.Println(l)
-	fmt.Errorf("Eror")
+	fmt.Println(arrayRevers)
 
 }
