e3998482b8c9c05ab450eebe4421d52216641e29
Nikita <nikita_chernay@ukr.net>
2022-06-15 22:16:02 +0300

    result2

--- Task1/main.go
+++ Task1/main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 var appleOne float64 = 5.99
 
-var peareOne int = 7
+var peareOne float64 = 7
 
-var myMoney int = 23
+var myMoney float64 = 23
 
 func main() {
 	firstQuestion(peareOne, appleOne)
@@ -16,29 +16,34 @@ func main() {
 
 }
 
-func firstQuestion(peareOne int, appleOne float64) {
+func firstQuestion(peareOne float64, appleOne float64) {
 	fmt.Println("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?")
 	fmt.Println("Нужно столько денег для груш", peareOne*8)
 	fmt.Println("Нужно столько денег для яблок", appleOne*9)
 
 }
 
-func secondQuestion(peareOne int, myMoney int) {
+func secondQuestion(peareOne float64, myMoney float64) {
 	fmt.Println("Скільки груш ми можемо купити?")
 	fmt.Println("Мы можем купить такое кол-во груш", myMoney/peareOne)
 
 }
 
-func thirdQuestion(appleOne float64, myMoney int) {
+func thirdQuestion(appleOne float64, myMoney float64) {
 	fmt.Println("Скільки яблук ми можемо купити?")
 	fmt.Println("Мы можем купить такое кол-во яблок", float64(myMoney)/float64(appleOne))
 
 }
 
-func fourthQuestion(peareOne int, appleOne float64) {
+func fourthQuestion(peareOne float64, appleOne float64) {
 	fmt.Println("Чи ми можемо купити 2 груші та 2 яблука?")
-	fmt.Println("Стоимость двух груш ", peareOne+peareOne)
-	fmt.Println("Стоимость двух яблок ", appleOne+appleOne)
-	fmt.Println("Мы не можем купить 2 груши и 2 яблока")
+	sumPeare := peareOne + peareOne
+	sumApple := appleOne + appleOne
+	sumAll := sumApple + sumPeare
+	if sumAll > myMoney {
+		fmt.Println("Мы не можем себе это позволить")
+	} else {
+		fmt.Println("Мы  можем себе это позволить")
+	}
 
 }
